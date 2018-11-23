cd84153622ee1c03f59c2472f67baf92986a4151
kanomdook <kanung.2535@gmail.com>
2018-11-23 10:28:48 +0700

    [add] page signin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'signin', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'signin', loadChildren: './pages/signin/signin.module#SigninPageModule' },
 ];
 
 @NgModule({
